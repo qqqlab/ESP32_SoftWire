162063b01c16c01caa8a4fd15df1964feb6e7482
qqqlab <46283638+qqqlab@users.noreply.github.com>
2023-12-19 18:36:27 +0100

    Update ESP32_SoftWire.ino

--- examples/ESP32_SoftWire/ESP32_SoftWire.ino
+++ examples/ESP32_SoftWire/ESP32_SoftWire.ino
@@ -5,34 +5,17 @@ SoftWire i2c;
 const int i2c_SDA_PIN  = 23; // 0-31
 const int i2c_SCL_PIN  = 22; // 0-31
 
-struct test_struct{
-    uint8_t adr;
-    uint8_t reg;
-    int expected;
-    char descr[40];
-};
-
-//table of address, register and expected who_am_i reply
-test_struct tests[] = {
-  {0x1E, 0x0A, 0x48, "HMC5883L ID-Reg-A 'H'"},
-  {0x1E, 0x0B, 0x34, "HMC5883L ID-Reg-B '4'"},
-  {0x1E, 0x0C, 0x33, "HMC5883L ID-Reg-C '3'"}, 
-  {0x68, 0x75, 0x68, "MPU6050"},
-  {0x69, 0x75, 0x68, "MPU6050"},
-  {0x68, 0x75, 0x68, "MPU9150"},
-  {0x69, 0x75, 0x68, "MPU9150"},  
-  {0x68, 0x75, 0x70, "MPU6500"},
-  {0x69, 0x75, 0x70, "MPU6500"}, 
-  {0x68, 0x75, 0x71, "MPU9250"},
-  {0x69, 0x75, 0x71, "MPU9250"},
-  {0x76, 0xD0, 0x56, "BMP280"},
-  {0x76, 0xD0, 0x57, "BMP280"},    
-  {0x76, 0xD0, 0x58, "BMP280"},
-  {0x76, 0xD0, 0x60, "BME280"},
-  {0x76, 0xD0, 0x61, "BMP680"},  
-  {0x77, 0, -1, "MS5611"},
-  {0, 0, 0, ""} //end
-};
+/* Example output
+===================
+ESP32 SoftWire Demo
+===================
+I2C: Scanning ...
+I2C: Found address: 0x1E (decimal 30)
+     adr=0x1E reg=0x0A -> 0x483433 ---> FOUND: HMC5883L
+I2C: Found address: 0x76 (decimal 118)
+     adr=0x76 reg=0xD0 -> 0x58 ---> FOUND: BMP280
+I2C: Found 2 device(s)
+*/
 
 void setup() {
   Serial.begin(115200);
@@ -42,25 +25,76 @@ void setup() {
 void loop() {
   Serial.println("===================\nESP32 SoftWire Demo\n===================");
 
-  i2c_scan();
-
-  int i = 0;
-  while(tests[i].adr) {
-    int adr = tests[i].adr;
-    int reg = tests[i].reg;
-    int expected = tests[i].expected;
-    int whoami = i2c_ReadReg(adr, reg);
-    Serial.printf("Read from adr 0x%02X reg 0x%02X -> value=0x%02X", adr, reg, whoami);
-    if(whoami == expected ) {
-      Serial.printf(" ---> MATCHED - ");
-    }else{
-      Serial.printf(" ---> expected 0x%02X - ", expected);
+  Serial.printf("I2C: Scanning ...\n");
+  int num_adr_found = 0;
+  i2c.begin();
+  for (uint8_t adr = 8; adr < 120; adr++) {
+    i2c.beginTransmission(adr);          // Begin I2C transmission Address (i)
+    if (i2c.endTransmission() == 0) { // Receive 0 = success (ACK response) 
+      Serial.printf("I2C: Found address: 0x%02X (decimal %d)\n",adr,adr);
+      num_adr_found++;
+      i2c_identify_chip(adr);
     }
-    Serial.println(tests[i].descr);
+  }
+  Serial.printf("I2C: Found %d device(s)\n", num_adr_found);
+  delay(1000);
+}
+
+struct test_struct{
+    uint8_t adr1;
+    uint8_t adr2;    
+    uint8_t reg;
+    int len;
+    uint32_t expected;
+    char descr[60];
+};
+
+//table of addresses, register and expected reply
+test_struct tests[] = {
+// adr1  adr2  reg  len exp       descr
+  {0x1E, 0x1E, 0x0A, 3, 0x483433, "HMC5883L"}, // ID-Reg-A 'H', ID-Reg-B '4', ID-Reg-C '3'
+  {0x68, 0x69, 0x00, 1, 0x68, "MPU3050"},
+  {0x68, 0x69, 0x00, 1, 0x69, "MPU3050"},
+  {0x68, 0x69, 0x75, 1, 0x68, "MPU6050"},
+  {0x68, 0x69, 0x75, 1, 0x72, "FAKE MPU6050, got WHO_AM_I=0x72, real chip returns 0x68"},
+  {0x68, 0x69, 0x75, 1, 0x19, "MPU6886"},
+  {0x68, 0x69, 0x75, 1, 0x68, "MPU9150"},
+  {0x68, 0x69, 0x75, 1, 0x70, "MPU6500"},
+  {0x68, 0x69, 0x75, 1, 0x71, "MPU9250"},
+  {0x68, 0x69, 0x75, 1, 0x98, "ICM-20689"},
+  {0x76, 0x76, 0xD0, 1, 0x56, "BMP280"},
+  {0x76, 0x76, 0xD0, 1, 0x57, "BMP280"},
+  {0x76, 0x76, 0xD0, 1, 0x58, "BMP280"},
+  {0x76, 0x76, 0xD0, 1, 0x60, "BME280"},
+  {0x76, 0x76, 0xD0, 1, 0x61, "BMP680"},
+  {0x40, 0x4F, 0x00, 2, 0x399F, "INA219"}, //adr 0x40-0x4F, reg 0x00->0x399F
+  {0x40, 0x4F, 0x00, 2, 0x4127, "INA226"}, //adr 0x40-0x4F, reg 0x00->0x4127
+  {0x48, 0x4F, 0x01, 2, 0x8583, "ADS1113, ADS1114, or ADS1115" },
+  {0x77, 0x77, 0, 0, 0, "MS5611"}, //identify by address match only 
+  {0,0,0,0,0,""} //end
+};
+
+void i2c_identify_chip(uint8_t adr) {
+  int i = 0;
+  while(tests[i].adr1) {
+    int adr1 = tests[i].adr1;
+    int adr2 = tests[i].adr2;    
+    int reg = tests[i].reg;
+    int len = tests[i].len;    
+    int expected = tests[i].expected;
+    uint32_t received = 0;
+    bool match = (len == 0 && adr1<=adr && adr<=adr2);
+    if(!match && adr1<=adr && adr<=adr2) {
+      uint8_t data[4];
+      i2c_ReadRegs(adr, reg, data, len);
+      for(int i=0;i<len;i++) received = (received<<8) + data[i];
+      match = (received == expected);
+    }
+    if(match) {
+      Serial.printf("     adr=0x%02X reg=0x%02X -> 0x%02X ---> FOUND: %s\n", adr, reg, received, tests[i].descr);
+    }
     i++;
   }
-
-  delay(1000);
 }
 
 void WriteReg( uint8_t adr, uint8_t reg, uint8_t data ) {
